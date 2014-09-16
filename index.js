b92c46c2511ad1f876850baa65682896550f1f0c
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2014-09-16 03:37:55 +0200

    Using NodeOS-mount-utils

--- index.js
+++ index.js
@@ -4,100 +4,51 @@ var fs = require('fs')
 
 var spawn = require('child_process').spawn
 
-var errno = require('src-errno');
 var mount = require('src-mount');
 
+var utils = require('nodeos-mount-utils');
 
-function mkdirMount(dev, path, type, flags, extras)
+
+function startRepl(error)
 {
-  if(typeof flags == 'string')
-  {
-    extras = flags
-    flags = undefined
-  }
+  console.warn(error)
 
-  flags = flags || null
-  extras = extras || ''
-
-  try
-  {
-    fs.mkdirSync(path)
-//    fs.mkdirSync(path, '0000')
-  }
-  catch(error)
-  {
-    if(error.code != 'EEXIST') throw error
-  }
-
-  var res = mount.mount(dev, path, type, flags, extras);
-  if(res == -1) console.error('Error '+errno.getErrorString()+' while mounting',path)
-  return res
-}
-
-function execInit(HOME)
-{
-  var homeStat = fs.statSync(HOME)
-
-  const initPath = HOME+'/init'
-
-  try
-  {
-    var initStat = fs.statSync(initPath)
-  }
-  catch(exception)
-  {
-    return initPath+' not found'
-  }
-
-  if(!initStat.isFile())
-    return initPath+' is not a file';
-
-  if(homeStat.uid != initStat.uid || homeStat.gid != initStat.gid)
-    return HOME+" uid & gid don't match with its init"
-
-  // Update env with user variables
-  var env =
-  {
-    HOME: HOME,
-    PATH: HOME+'/bin:/usr/bin',
-    __proto__: process.env
-  }
-
-  // Start user's init
-  spawn(initPath, [],
-  {
-    cwd: HOME,
-    stdio: 'inherit',
-    env: env,
-    detached: true,
-    uid: homeStat.uid,
-    gid: homeStat.gid
-  });
+  utils.startRepl('NodeOS-usersfs')
 }
 
 
-var USERS = process.env.USERS
-if(USERS)
+// Mount users filesystem
+
+var envDev = 'USERS'
+var path   = '/home';
+var type   = 'ext4' //process.env.ROOTFSTYPE || 'auto';
+var extras = '';
+
+utils.mountfs(envDev, path, type, extras, function(error)
 {
-  var dev  = USERS;
-  var path = '/home';
-  var type = 'ext4';
-
-  var res = mkdirMount(dev, path, type);
-
-  if(res == 0)
+  if(!error)
   {
-    delete process.env.USERS
+    // Running on Docker?
+    try
+    {
+      fs.statSync('/.dockerinit')
+      var cmd = '/home/nodeos/bin/nsh'
+    }
+    catch(err)
+    {
+      var cmd = 'forever-starter'
+    }
 
-    spawn('forever-starter', [],
+    spawn(cmd, [],
     {
       stdio: 'inherit',
       detached: true
-    });
+    })
+    .on('error', startRepl)
 
-    return fs.readdir(path, function(err, files)
+    return fs.readdir(path, function(error, files)
     {
-      if(err) return console.warn(err)
+      if(error) return startRepl(error)
 
       files.forEach(function(file)
       {
@@ -110,24 +61,15 @@ if(USERS)
           if(!stats.isDirectory())
             return console.warn(HOME+' is not a directory');
 
-          var error = execInit(HOME)
+          var error = utils.execInit(HOME)
           if(error)
             console.warn(error)
         })
       })
     })
   }
-}
-else
-  console.warning('USERS filesystem not defined')
 
+  // Error mounting the users filesystem, enable REPL
 
-// Error booting, enable REPL
-
-console.log('Starting REPL session')
-
-require("repl").start("NodeOS-usersfs> ").on('exit', function()
-{
-  console.log('Got "exit" event from repl!');
-  process.exit(2);
-});
+  startRepl(error)
+})
